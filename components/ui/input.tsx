147a0045839413b60c244f778f4698145eae476b
Mohamed Hedeya <mohamedhadia611@gmail.com>
2023-05-05 16:53:00 +0300

    feat(input): add error state for input

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -7,10 +7,11 @@ import { cn } from "@/lib/utils"
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {
   Icon?: React.ElementType
+  error?: boolean
 }
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, Icon, ...props }, ref) => {
+  ({ className, type, Icon, error, ...props }, ref) => {
     return (
       <div className="relative">
         {Icon && (
@@ -22,7 +23,8 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
           type={type}
           className={cn(
             "flex h-10 w-full rounded-md bg-gray-100 px-3 py-2 text-right text-lg font-bold text-secondary ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-right focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
-            className
+            className,
+            error && "ring-2 ring-red-500 focus-visible:ring-red-500"
           )}
           ref={ref}
           {...props}
